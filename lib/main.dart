7a7d4beb6e47d64753488505e905ed37d867b00c
fen0268 <>
2024-05-09 12:56:27 +0900

    feat: providerScope 追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'config/app.dart';
 import 'config/firebase_options.dart';
+import 'config/preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const App());
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        //インスタンス取得は非同期なので初回に取得してキャッシュしておく
+        sharedPreferencesProvider.overrideWithValue(
+          await SharedPreferences.getInstance(),
+        ),
+      ],
+      child: const App(),
+    ),
+  );
 }
