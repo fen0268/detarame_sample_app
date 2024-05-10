837dbc27e3c74e07343941517ab8c3e7bfd59102
fen0268 <>
2024-05-10 19:52:41 +0900

    feat: paypay 決済画面に遷移する実装

---
+++ lib/feature/payment/domain/transition_url.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'transition_url.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$TransitionUrlImpl _$$TransitionUrlImplFromJson(Map<String, dynamic> json) =>
+    _$TransitionUrlImpl(
+      transitionUrl: json['transitionUrl'] as String,
+    );
+
+Map<String, dynamic> _$$TransitionUrlImplToJson(_$TransitionUrlImpl instance) =>
+    <String, dynamic>{
+      'transitionUrl': instance.transitionUrl,
+    };
